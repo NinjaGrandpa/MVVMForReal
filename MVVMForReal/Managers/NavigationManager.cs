0fcf7bcc1b32a59a4459b7aab5aafaac523b2d80
NinjaGrandpa <34811547+NinjaGrandpa@users.noreply.github.com>
2023-12-19 12:38:30 +0100

    Created NavigationManager

---
+++ MVVMForReal/Managers/NavigationManager.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MVVMForReal.Managers;
+
+public class NavigationManager : INavigationManager
+{
+    public ObservableObject CurrentViewModel { get; set; }
+    public event Action? CurrentViewModelChanged;
+
+    public void OnCurrentViewModelChanged()
+    {
+        CurrentViewModelChanged?.Invoke();
+    }
+}
